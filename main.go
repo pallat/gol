d22d44567ec4a20e88c2459f510da02df801edaf
pallat <yod.pallat@gmail.com>
2014-11-16 11:31:07 +0700

    count around cell

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	// "fmt"
+)
+
 const (
 	LIVE = true
 	DEAD = false
@@ -10,7 +14,6 @@ type life struct {
 }
 
 func (l *life)rule(neighbours int) {
-
 	if l.alive == DEAD {
 		if neighbours == 3 {
 			l.alive = LIVE
@@ -22,6 +25,41 @@ func (l *life)rule(neighbours int) {
 			l.alive = DEAD
 		}
 	}
+}
+
+type table [][]bool
+
+func (t *table) born(x, y int) {
+	[][]bool(*t)[x][y] = true
+}
+
+func(t *table) around(x, y int) int {
+	xmin := 0
+	ymin := 0
+
+	count :=0
+	if x > 0 {xmin = x-1}
+	if y > 0 {ymin = y-1}
+
+	for xx:=xmin;xx<=x+1;xx++ {
+		for yy:=ymin;yy<=y+1;yy++ {
+			if 	[][]bool(*t)[xx][yy] != true {
+				count +=1
+			}
+
+		}
+	}
+
+	return count
+}
+
+func spaces(height int, weight int) table {
+	var t table
+	for i:=0;i<height;i++ {
+		t = append(t,make([]bool,weight))
+	}
+
+	return t
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -57,3 +57,23 @@ func TestRule4DeadCellShouldBeStillDeadWhenTwoNeighboursHasAlive(t *testing.T) {
 		t.Errorf("Expected cell died but it alive.")	
 	}
 }
+
+func TestTotal8Around(t *testing.T) {
+	spaces := spaces(3,3)
+	spaces.born(1,1)
+	total := spaces.around(1,1)
+
+	if total != 8 {
+		t.Errorf("Expected 8 around but was %i", total)
+	}
+}
+
+func TestTotal3Around(t *testing.T) {
+	spaces := spaces(3,3)
+	spaces.born(0,0)
+	total := spaces.around(0,0)
+
+	if total != 3 {
+		t.Errorf("Expected 3 around but was %i", total)
+	}
+}
